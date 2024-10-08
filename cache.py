ea3ff39f6fbad5d41f78877f55b4344b5b2a3aba
waltertaya <waltermittybecker5@gmail.com>
2024-10-09 00:14:31 +0300

    Updated cache management logic in cache.py

--- cache.py
+++ cache.py
@@ -1,1 +1,21 @@
-# Cache management logic
+class Cache:
+    ''' Implements a cache for the cache proxy server '''
+    def __init__(self):
+        ''' Initializes the cache '''
+        self.cache = {}
+    
+    def exists(self,url):
+        ''' Checks if the url exists in the cache '''
+        return url in self.cache
+    
+    def get(self,url):
+        ''' Gets the url from the cache '''
+        return self.cache.get(url)
+    
+    def set(self,url,response):
+        ''' Sets the url in the cache '''
+        self.cache[url] = response
+
+    def clear(self):
+        ''' Clears the cache '''
+        self.cache = {}
